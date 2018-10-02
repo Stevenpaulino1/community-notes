f699faafe1d43c209095f5387ad3ae246f60a77f
github username <github email address>
2018-10-02 15:43:32 -0400

    Set schema

--- db/migrate/20181002133318_create_topics.rb
+++ db/migrate/20181002133318_create_topics.rb
@@ -1,8 +1,9 @@
 class CreateTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :topics do |t|
-      t.text :name
-
+      t.string :name
+      t.text :description
+      t.integer :category_id
       t.timestamps
     end
   end
--- db/migrate/20181002134234_create_gifs.rb
+++ db/migrate/20181002134234_create_gifs.rb
@@ -1,7 +1,10 @@
 class CreateGifs < ActiveRecord::Migration[5.2]
   def change
     create_table :gifs do |t|
-
+      t.string :name
+      t.integer :user_id
+      t.string :link
+      t.integer :user_entry_id
       t.timestamps
     end
   end
